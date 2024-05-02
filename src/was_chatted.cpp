5e18c8c21d8a540266a0f1900e454d1f42dad037
PC <pompeu@ua.pt>
2024-05-02 15:09:12 +0100

    rewrite was_chatted

--- src/was_chatted.cpp
+++ src/was_chatted.cpp
@@ -4,34 +4,112 @@
 #include <cmath>
 #include <unordered_map>
 #include <algorithm>
-#include <set>
 #include <numeric>
 #include <chrono>
-
-using namespace std;
+#include <memory>
+#include <sstream>
 
 size_t k = 3, alpha = 1;
 
-bool isValidFile(const string& filename) {
-    ifstream file(filename);
+class ModelData {
+    private:
+        std::unordered_map<std::string,std::vector<size_t>> counts;
+        const std::shared_ptr<std::unordered_map<char,size_t>> alphabet;
+        const double alpha;
+
+        size_t symbolPosition(char symbol) const {
+            return alphabet->at(symbol);
+        }
+
+    public:
+        ModelData(const std::shared_ptr<std::unordered_map<char,size_t>>& _alphabet,const double _alpha): alphabet(_alphabet),alpha(_alpha) {}
+
+        void increment(const std::string& context,char symbol) {
+            if(counts.find(context) == counts.end()) {
+                counts[context] = std::vector<size_t>(alphabet->size(),0);
+            }
+            counts[context][symbolPosition(symbol)]++;
+        }
+
+        virtual double estimateProbability(char symbol,const std::string& context) {
+            if(counts.find(context) == counts.end())
+                return alpha;
+
+            std::vector<size_t>& data = counts.at(context);
+            size_t symbolCount = data.at(symbolPosition(symbol));
+            size_t sum = accumulate(data.begin(),data.end(),0);
+
+            return (symbolCount + alpha) / (sum + alpha * alphabet->size());
+        }
+};
+
+class MarkovModel {
+    private:
+        size_t contextSize = 1;
+        char mostFrequent;
+        std::unordered_map<char,size_t> counts;
+    protected:
+        ModelData modelData;
+
+        virtual char findMostFrequent() {
+            return max_element(counts.begin(),counts.end(),[](const std::pair<char,size_t>& a,const std::pair<char,size_t>& b) {
+                    return a.second < b.second;
+                })->first;
+        }
+    public:
+        virtual void train(const std::string& data) {
+            for(size_t i = 0; i < data.size() - contextSize; i++) {
+                std::string context = data.substr(i,contextSize);
+                char symbol = data[i + contextSize];
+                modelData.increment(context,symbol);
+                counts[symbol]++;
+            }
+
+            mostFrequent = findMostFrequent();
+        }
+
+        virtual double calculateBits(const std::string& text) {
+            std::string extraBits(contextSize,mostFrequent);
+            double bits = 0;
+            std::string context(contextSize,mostFrequent);
+            for(size_t i = 0; i < text.size(); i++) {
+                char symbol = text[i];
+                double prob = modelData.estimateProbability(symbol,context);
+                bits += -log2(prob);
+                context = context.substr(1) + symbol;
+            }
+
+            return bits;
+        }
+
+        MarkovModel(size_t _contextSize,const double _alpha,const std::shared_ptr<std::unordered_map<char,size_t>>& _alphabet): contextSize(_contextSize),modelData(_alphabet,_alpha) {}
+        virtual ~MarkovModel() {}
+};
+
+bool isValidFile(const std::string& filename) {
+    std::ifstream file(filename);
     if (!file.is_open()) {
-        cerr << "Cannot open file: " << filename << endl;
+        std::cerr << "Cannot open file: " << filename << std::endl;
         return false;
     }
     file.close();
     return true;
 }
 
-vector<char> Alphabet(const string& inputFilename) {
-    ifstream inputFile(inputFilename);
-    vector<char> alphabet;
+std::unordered_map<char,size_t> alphabet(const std::string& inputFilename) {
+    std::ifstream inputFile(inputFilename);
+
+    std::unordered_map<char,size_t> alphabet;
+
     char symbol;
+    size_t counter = 0;
 
     while (inputFile.get(symbol)) {
         // Verificar se o caractere já está no alfabeto
-        if (find(alphabet.begin(), alphabet.end(), symbol) == alphabet.end()) {
+        if (alphabet.find(symbol) == alphabet.end()) {
             // Se não estiver, adicioná-lo ao alfabeto
-            alphabet.push_back(symbol);
+            alphabet[symbol] = counter;
+            counter++;
         }
     }
 
@@ -39,77 +117,25 @@ vector<char> Alphabet(const string& inputFilename) {
     return alphabet;
 }
 
-unordered_map<string, vector<int>> FCM(const string& filename, const int k, const vector<char>& alphabet) {
-    unordered_map<string, vector<int>> model;
-    ifstream file(filename);
-    string line;
-    string context;
+MarkovModel FCM(const std::string& filename, size_t k, const std::shared_ptr<std::unordered_map<char,size_t>>& alphabet) {
+    #define MAX_BUFFER_SIZE 8000
+
+    MarkovModel model(k,alpha,alphabet);
+    std::ifstream file(filename);
+
+    char* charBuffer = new char[MAX_BUFFER_SIZE];
+    std::string data;
     
-    while (getline(file, line)) {
-        line.erase(remove_if(line.begin(), line.end(), ::isspace), line.end()); // Remove whitespace
-        if (line.size() < k + 1) continue; // Skip lines shorter than k + 1
-        
-        // Iterate through each character in the line
-        for (size_t i = 0; i <= line.size() - k - 1; ++i) {
-            context = line.substr(i, k); // Extract context of length k
-            char symbol = line[i + k]; // Extract the symbol after the context
-
-            // If the context is not in the model yet, add it
-            if (model.find(context) == model.end()) {
-                model[context] = vector<int>(alphabet.size(), 0);
-            }
-
-            // Increment count for the symbol in this context
-            size_t symbolIndex = distance(alphabet.begin(), find(alphabet.begin(), alphabet.end(), symbol));
-            if (symbolIndex != alphabet.size()) { // Check if symbol exists in the alphabet
-                ++model[context][symbolIndex];
-            }
-        }
+    while (file.readsome(charBuffer,MAX_BUFFER_SIZE)) {
+        data.assign(charBuffer,file.gcount());
+        model.train(data);
     }
+
+    delete charBuffer;
     
     file.close();
     return model;
 }
-
-
-// Função para determinar a que classe o texto pertence
-// Com base nos resultados obtidos no modelo fcm
-// Verificar a qual das classes o texto em analise pertence
-string determinateClass(const unordered_map<string, vector<int>>& resultsHuman, const unordered_map<string, vector<int>>& resultsGpt, const string& textFile, const int k, const double alpha) {
-    double humanScore = 0.0;
-    double gptScore = 0.0;
-    ifstream file(textFile);
-    string line;
-
-    while (getline(file, line)) {
-        for (size_t i = 0; i <= line.size() - k - 1; i++) {
-            string context = line.substr(i, k);
-            char nextSymbol = line[i + k];
-
-            // Check if context is in the maps
-            if (resultsHuman.find(context) != resultsHuman.end() && resultsGpt.find(context) != resultsGpt.end()) {
-                // Calculate probability for human model
-                double probHuman = (resultsHuman.at(context)[nextSymbol] + alpha) / (accumulate(resultsHuman.at(context).begin(), resultsHuman.at(context).end(), 0) + alpha * resultsHuman.at(context).size());
-                humanScore -= log2(probHuman);
-
-                // Calculate probability for GPT model
-                double probGpt = (resultsGpt.at(context)[nextSymbol] + alpha) / (accumulate(resultsGpt.at(context).begin(), resultsGpt.at(context).end(), 0) + alpha * resultsGpt.at(context).size());
-                gptScore -= log2(probGpt);
-            }
-        }
-    }
-
-    file.close();
-
-    cout << "Human score: " << humanScore << endl;
-    cout << "GPT score: " << gptScore << endl;
-    if (humanScore < gptScore) {
-        return "Human";
-    } else {
-        return "GPT";
-    }
-}
-
 
 int main(int argc,char* argv[]) {
     /*argumentos:
@@ -122,7 +148,7 @@ int main(int argc,char* argv[]) {
         5 alfa parametro de suavização
         ....
     */
-    auto start = chrono::high_resolution_clock::now(); 
+    auto start = std::chrono::high_resolution_clock::now(); 
     
     if (argc != 4) {
         std::cerr << "Usage: "<< argv[0] << "<human_collection> <gpt_collection> <text_to_analyse>" << std::endl;
@@ -135,29 +161,49 @@ int main(int argc,char* argv[]) {
 
     // Pode se especificar qual/quais tao incorretos
     // Verificar se os ficheiros de textos fornecidos são válidos
-    cout << "Checking files..." << endl;
+    std::cout << "Checking files..." << std::endl;
     if (!isValidFile(humanCollectionFile) || !isValidFile(gptCollectionFile) || !isValidFile(textFile)) {
         std::cerr << "Invalid file(s) provided!" << std::endl;
         exit(EXIT_FAILURE);
     }
     
     // Obter o alfabeto do ficheiro de entrada
-    cout << "Obtaining alphabet..." << endl;
-    std::vector<char> alphabetHuman = Alphabet(humanCollectionFile);
-    std::vector<char> alphabetGpt = Alphabet(gptCollectionFile);
+    std::cout << "Obtaining alphabet..." << std::endl;
+    std::shared_ptr<std::unordered_map<char,size_t>> alphabetHuman = std::make_shared<std::unordered_map<char,size_t>>(alphabet(humanCollectionFile));
+    std::shared_ptr<std::unordered_map<char,size_t>> alphabetGpt = std::make_shared<std::unordered_map<char,size_t>>(alphabet(gptCollectionFile));
     
-    cout << "Applying FCM..." << endl;
-    unordered_map<string, vector<int>> resultsHuman = FCM(humanCollectionFile, k, alphabetHuman);
-    unordered_map<string, vector<int>> resultsGpt = FCM(gptCollectionFile, k, alphabetGpt);
+    std::cout << "Applying FCM..." << std::endl;
+    MarkovModel resultsHuman = FCM(humanCollectionFile, k, alphabetHuman);
+    MarkovModel resultsGpt = FCM(gptCollectionFile, k, alphabetGpt);
 
-    cout << "Determining class..." << endl;
-    // Determinar a que classe o texto pertence
-    string result = determinateClass(resultsHuman, resultsGpt, textFile, k, alpha);
-    cout << "The text belongs to the class: " << result << endl;
+    std::ifstream file(textFile);
+    if(!file.is_open()) {
+        std::cerr << "Error opening file: " << textFile << std::endl;
+        return 1;
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    const std::string text(buffer.str());
+
+    std::cout << "Determining class..." << std::endl;
+    double bitsHuman = resultsHuman.calculateBits(text);
+    double bitsGPT = resultsGpt.calculateBits(text);
+    std::string res;
+
+    std::cout << "Human score: " << bitsHuman << std::endl;
+    std::cout << "GPT score: " << bitsGPT << std::endl;
+
+    if(bitsHuman < bitsGPT)
+        res = "Human";
+    else
+        res = "GPT";
+
+    std::cout << "The text belongs to the class: " << res << std::endl;
 
     auto now = std::chrono::high_resolution_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - start).count();
-    cout << "Elapsed time: " << elapsed << " seconds" << endl;
+    std::cout << "Elapsed time: " << elapsed << " seconds" << std::endl;
 
     return 0;
 }
