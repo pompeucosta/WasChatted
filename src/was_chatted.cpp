b73547871656f4ed81fcf122b171b87f849ae635
RafaelPBPinto <rafaelpbpinto@ua.pt>
2024-05-06 19:57:10 +0100

    revert changes

--- src/was_chatted.cpp
+++ src/was_chatted.cpp
@@ -208,19 +208,20 @@ int main(int argc,char* argv[]) {
         std::cerr << "Invalid file(s) provided!" << std::endl;
         exit(EXIT_FAILURE);
     }
-    MarkovModel resultsHuman(k,alpha);
-    MarkovModel resultsGpt(k,alpha);
+
+    // Obter o alfabeto do ficheiro de entrada
+    std::cout << "Obtaining alphabet..." << std::endl;
+    std::shared_ptr<std::unordered_map<char,size_t>> alphabetHuman = std::make_shared<std::unordered_map<char,size_t>>(alphabet(humanCollectionFile));
+    std::shared_ptr<std::unordered_map<char,size_t>> alphabetGpt = std::make_shared<std::unordered_map<char,size_t>>(alphabet(gptCollectionFile));
+
+    std::cout << "Applying FCM..." << std::endl;
+    MarkovModel resultsHuman(k,alpha,alphabetHuman);
+    MarkovModel resultsGpt(k,alpha,alphabetGpt);
     
     if (existsFile("./resultsHuman.bin") && existsFile("./resultsGpt.bin")) {
-        std::cout << "Applying FCM..." << std::endl;
         resultsHuman.getData("./resultsHuman.bin");
         resultsGpt.getData("./resultsGpt.bin");
     } else {
-        // Obter o alfabeto do ficheiro de entrada
-        std::cout << "Obtaining alphabet..." << std::endl;
-        std::shared_ptr<std::unordered_map<char,size_t>> alphabetHuman = std::make_shared<std::unordered_map<char,size_t>>(alphabet(humanCollectionFile));
-        std::shared_ptr<std::unordered_map<char,size_t>> alphabetGpt = std::make_shared<std::unordered_map<char,size_t>>(alphabet(gptCollectionFile));
-        
         FCM(resultsHuman, humanCollectionFile, k, alphabetHuman);
         FCM(resultsGpt, gptCollectionFile, k, alphabetGpt);
         resultsHuman.saveData("./resultsHuman.bin");
